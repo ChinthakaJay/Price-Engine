3f9550d5199a4501e5598cc8e0053a94413172c8
Chinthaka <chinthaka.jayarathne@axiatadigitallabs.com>
2021-06-22 14:48:23 +0530

    Extract Project

---
+++ src/test/java/com/chinthaka/priceengine/PriceengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinthaka.priceengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
