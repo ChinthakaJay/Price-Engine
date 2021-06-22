cbea8bca97a6e3a120dd236994f67f3a8f8ebe0d
Chinthaka <chinthaka.jayarathne@axiatadigitallabs.com>
2021-06-22 14:50:55 +0530

    Correct file names

--- src/main/java/com/chinthaka/priceengine/PriceEngineApplication.java
+++ src/main/java/com/chinthaka/priceengine/PriceEngineApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PriceengineApplication {
+public class PriceEngineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PriceengineApplication.class, args);
+		SpringApplication.run(PriceEngineApplication.class, args);
 	}
 
 }
